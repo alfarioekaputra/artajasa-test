c5e052bda4cf74c79994f95a4594a88d742163ae
Rio <alfarioekaputra@gmail.com>
2025-07-16 23:02:11 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+	engine := html.New("./views", ".html")
+	engine.Reload(true)
+
+	app := fiber.New(fiber.Config{
+		Prefork:       true,
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "Fiber",
+		Views:         engine,
+	})
+
+	app.Static("/", "./public")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		// Render index
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+
+	app.Listen(":8000")
+}
